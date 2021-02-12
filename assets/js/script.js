ad3e685d788261fb49b4a90b3b3efb963ee0c6ce
Jeff Mead <jeffdmead171@gmail.com>
2021-02-12 15:58:10 -0500

    fix bugs and bootstrap spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 let displayOrder = (e) => {
-    if (e.target.id === "cb") {
-        let price = 7.00;
-        let foodEl = $('<p>').text(`Cheese Burger $ ${price}`).addClass('.fs-6');
-        $('#order').append(foodEl);
-    };
+    let foodEl = "";
+    let price = 0;
+    switch (e.target.id) {
+        case "cb":
+            price = 8.00;
+            foodEl = $('<p>').text(`Cheese Burger $ ${price}`).addClass('.fs-6');
+            $('#order').append(foodEl);
+            break;
+        case "cs":
+            price = 7.00;
+            foodEl = $('<p>').text(`Chicken Ceasar $ ${price}`).addClass('.fs-6');
+            $('#order').append(foodEl);
+            break;
+        default:
+            break;
+    }
 }
 
 $('button').click( function(e) {
