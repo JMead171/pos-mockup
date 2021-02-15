4c3519f69dc45edcde0f82f489697606cd732efe
Jeff Mead <jeffdmead171@gmail.com>
2021-02-15 16:09:43 -0500

    added total order logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,31 @@
 let displayOrder = (e) => {
     let foodEl = "";
     let price = 0;
+    let totalPrice = 0;
     switch (e.target.id) {
         case "cb":
-            price = 8.00;
-            foodEl = $('<p>').text(`Cheese Burger $ ${price}`).addClass('.fs-6');
+            price = 8.95;
+            foodEl = $('<p>').text(`Cheese Burger $${price}`).addClass('.fs-6 orders');
             $('#order').append(foodEl);
             break;
         case "cs":
-            price = 7.00;
-            foodEl = $('<p>').text(`Chicken Ceasar $ ${price}`).addClass('.fs-6');
+            price = 7.95;
+            foodEl = $('<p>').text(`Chicken Ceasar $${price}`).addClass('.fs-6 orders');
             $('#order').append(foodEl);
             break;
         default:
             break;
     }
+    $('.total-price').remove();
+    let orderPrices = $('.orders');
+    for ( let i = 0; i < orderPrices.length; i++) {
+        let str = orderPrices[i].innerHTML;
+        let value = str.slice(-4);
+        totalPrice += parseFloat(value);
+        // console.log(totalPrice.toFixed(2));
+    }
+    foodEl = $('<p>').text(`Your order total comes to $${totalPrice.toFixed(2)}`).addClass('h6 total-price');
+    $('#total').append(foodEl);
 }
 
 $('button').click( function(e) {
