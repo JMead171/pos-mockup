cec98cf30480edd86d149b2ecf0745939dd2a40a
Jeff Mead <jeffdmead171@gmail.com>
2021-02-16 14:24:31 -0500

    added spacing and menu items

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ POS mock up
 
 ## Description
 This project is a mock-up of a POS system for a food & beverage business.
+- It shows items to be ordered
+- User can click on food item to add to order
+- Order is displayed on the right side of screen with prices
 
 
   
@@ -35,7 +38,7 @@ JavaScript, HTML5, CSS, jQuery and Bootstrap.
 
 
 ## Screen Shot
-  
+  ![image](https://user-images.githubusercontent.com/64744763/108106872-84aa7d80-705c-11eb-8908-d6a855b56391.png)
   
 ## Questions
 Please send questions to jeffdmead171@gmail.com, more information can be found at https://github.com/JMead171/pos-mockup
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,62 @@ let displayOrder = (e) => {
     switch (e.target.id) {
         case "cb":
             price = 8.95;
-            foodEl = $('<p>').text(`Cheese Burger $${price}`).addClass('.fs-6 orders');
+            foodEl = $('<p>').text(`Cheese Burger $${price}`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "cs":
             price = 7.95;
-            foodEl = $('<p>').text(`Chicken Ceasar $${price}`).addClass('.fs-6 orders');
+            foodEl = $('<p>').text(`Chicken Sandwich $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "blt":
+            price = 7.95;
+            foodEl = $('<p>').text(`BLT Sandwich $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "cp":
+            price = 6.95;
+            foodEl = $('<p>').text(`Cheese Pizza $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "cc":
+            price = 7.95;
+            foodEl = $('<p>').text(`Chicken Caesar $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "ch":
+            price = 5.95;
+            foodEl = $('<p>').text(`Chilli $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "ss":
+            price = 9.95;
+            foodEl = $('<p>').html(`Steak Sandwich <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "ft":
+            price = 8.95;
+            foodEl = $('<p>').text(`Fish Tacos    $${price}`).addClass('fs-6 orders bg-light bg-light tab4');
+            $('#order').append(foodEl);
+            break;
+        case "hs":
+            price = 5.95;
+            foodEl = $('<p>').text(`House Salad $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "so":
+            price = 2.95;
+            foodEl = $('<p>').text(`Soft Drink $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "be":
+            price = 4.95;
+            foodEl = $('<p>').text(`Beer $${price}`).addClass('fs-6 orders bg-light');
+            $('#order').append(foodEl);
+            break;
+        case "wi":
+            price = 6.95;
+            foodEl = $('<p>').text(`Wine $${price}`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         default:
@@ -19,12 +69,12 @@ let displayOrder = (e) => {
     $('.total-price').remove();
     let orderPrices = $('.orders');
     for ( let i = 0; i < orderPrices.length; i++) {
-        let str = orderPrices[i].innerHTML;
+        console.log("Look ", orderPrices);
+        let str = orderPrices[i].innerText;
         let value = str.slice(-4);
         totalPrice += parseFloat(value);
-        // console.log(totalPrice.toFixed(2));
     }
-    foodEl = $('<p>').text(`Your order total comes to $${totalPrice.toFixed(2)}`).addClass('h6 total-price');
+    foodEl = $('<p>').html(`Your order total comes to <span class='tab4'>$${totalPrice.toFixed(2)}</span>`).addClass('h6 total-price');
     $('#total').append(foodEl);
 }
 
