999d27045bac1231b109cc5243adf79aecf7f06e
Jeff Mead <jeffdmead171@gmail.com>
2021-02-17 16:23:08 -0500

    added styling, clear and submit button logic

--- README.md
+++ README.md
@@ -38,7 +38,8 @@ JavaScript, HTML5, CSS, jQuery and Bootstrap.
 
 
 ## Screen Shot
-  ![image](https://user-images.githubusercontent.com/64744763/108106872-84aa7d80-705c-11eb-8908-d6a855b56391.png)
-  
+  ![image](https://user-images.githubusercontent.com/64744763/108269572-2e147080-713c-11eb-9958-30730d96ed66.png)
+
+
 ## Questions
 Please send questions to jeffdmead171@gmail.com, more information can be found at https://github.com/JMead171/pos-mockup
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,105 @@
 let displayOrder = (e) => {
     let foodEl = "";
-    let price = 0;
+    let priceEl = e.target.previousElementSibling.previousElementSibling.innerText;
+    let price = parseFloat(priceEl.replace('$', ""));
     let totalPrice = 0;
     switch (e.target.id) {
         case "cb":
-            price = 8.95;
-            foodEl = $('<p>').text(`Cheese Burger $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Cheese Burger <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "cs":
-            price = 7.95;
-            foodEl = $('<p>').text(`Chicken Sandwich $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Chicken Sandwich <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "blt":
-            price = 7.95;
-            foodEl = $('<p>').text(`BLT Sandwich $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`BLT Sandwich <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "cp":
-            price = 6.95;
-            foodEl = $('<p>').text(`Cheese Pizza $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Cheese Pizza <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "cc":
-            price = 7.95;
-            foodEl = $('<p>').text(`Chicken Caesar $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Chicken Caesar <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "ch":
-            price = 5.95;
-            foodEl = $('<p>').text(`Chilli $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Chilli <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "ss":
-            price = 9.95;
             foodEl = $('<p>').html(`Steak Sandwich <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "ft":
-            price = 8.95;
-            foodEl = $('<p>').text(`Fish Tacos    $${price}`).addClass('fs-6 orders bg-light bg-light tab4');
+            foodEl = $('<p>').html(`Fish Tacos <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "hs":
-            price = 5.95;
-            foodEl = $('<p>').text(`House Salad $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`House Salad <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "so":
-            price = 2.95;
-            foodEl = $('<p>').text(`Soft Drink $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Soft Drink <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "be":
-            price = 4.95;
-            foodEl = $('<p>').text(`Beer $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Beer <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         case "wi":
-            price = 6.95;
-            foodEl = $('<p>').text(`Wine $${price}`).addClass('fs-6 orders bg-light');
+            foodEl = $('<p>').html(`Wine <span class='tab4'>$${price}</span>`).addClass('fs-6 orders bg-light');
             $('#order').append(foodEl);
             break;
         default:
             break;
     }
     $('.total-price').remove();
+    $('.clear').remove();
     let orderPrices = $('.orders');
     for ( let i = 0; i < orderPrices.length; i++) {
-        console.log("Look ", orderPrices);
         let str = orderPrices[i].innerText;
         let value = str.slice(-4);
         totalPrice += parseFloat(value);
     }
-    foodEl = $('<p>').html(`Your order total comes to <span class='tab4'>$${totalPrice.toFixed(2)}</span>`).addClass('h6 total-price');
+    foodEl = $('<p>').html(`Your order total: <span class='tab4'>$${totalPrice.toFixed(2)}</span>`).addClass('h6 bg-secondary text-white total-price');
     $('#total').append(foodEl);
+    let divEl = $('<div id="reset">').addClass('clear');
+    $('#total').append(divEl);
+    let buttonEl = $(`<button type='button'>`).html(`Clear Order`).addClass('btn btn-danger btn-sm btnClear ');
+    $('.clear').append(buttonEl);
+    let buttonE2 = $(`<button type='button'>`).html(`Submit Order`).addClass('btn btn-warning btn-sm btnSubmit');
+    $('.clear').append(buttonE2);
+
 }
 
 $('button').click( function(e) {
     displayOrder(e);
 });
+
+$(document).on('click', '.btnClear', function(){
+    window.location.reload()
+});
+
+$(document).on('click', '.btnSubmit', function(){
+    let date = new Date()
+    let mon = date.getMonth();
+    let month = mon + 1;
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let hour ="", min = "", sec = "";
+    let hstr = h.toString();
+    let mstr = m.toString();
+    let sstr = s.toString()
+    hstr.length < 2 ? hour = "0" + hstr : hour = hstr;
+    mstr.length < 2 ? min = "0" + mstr : min = mstr;
+    sstr.length < 2 ?  sec = "0" + sstr : sec = sstr;
+    let orderDate = `${month}/${day}/${year} ${hour}:${min}:${sec}` 
+    let orderEl = $(`<p>`).html(`Your order was sent to the kitchen on ${orderDate}`).addClass('orderSent');
+    $('.clear').append(orderEl);
+});
+       
